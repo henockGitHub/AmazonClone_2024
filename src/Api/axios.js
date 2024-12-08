17e2249e73ecd12d437ad5fe0097c0aac27c2b62
HeniMan <henock2000@gmail.com>
2024-12-08 14:54:50 +0100

    update baseurl

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -4,8 +4,9 @@ const axiosInstance = axios.create({
   // firebase deployed
   // baseURL : "https://api-gzaowg74wq-uc.a.run.app"
   //  using render
-  // baseURL: "http://127.0.0.1:5001/clone-1cb13/us-central1/api",
-  baseURL: "http://localhost:5000",
+  baseURL: "https://amazon-api-94c5.onrender.com",
+  // localhost
+  // baseURL: "http://localhost:5000",
 });
 
 export { axiosInstance };
